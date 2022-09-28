8e24abdfa6eed4bed4b4c8b503056cb9e42422d9
rk <rktheaver@gmail.com>
2022-09-28 20:14:22 +0530

    Initial commit

---
+++ abell.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sourcePath: 'theme',
+  destinationPath: 'dist',
+  contentPath: 'content',
+  globalMeta: {
+    siteName: 'Abell Minima',
+    twitter: 'AbellLand',
+    domain: 'https://abell-starter-minima.netlify.app'
+  }
+}
